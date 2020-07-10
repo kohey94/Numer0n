6efb13d10ee7c55e62560327e5c3622f0cdee42e
KoHey94@X1C <sugawara622@gmail.com>
2020-07-10 23:47:05 +0900

    乱数生成処理追加

--- Numer0n/Numer0n.cs
+++ Numer0n/Numer0n.cs
@@ -1,9 +1,29 @@
-﻿/// <summary>
+﻿using System;
+using System.Linq;
+
+/// <summary>
 /// Numer0nのロジックを書いていく予定
 /// </summary>
 namespace Numer0n
 {
-    class Numer0n
+    static class Numer0n
     {
+        static string GenerateRandomData()
+        {
+            var rnd = new Random();
+            while (true)
+            {
+                char[] randomData = {
+                    Convert.ToChar(rnd.Next(10).ToString()),
+                    Convert.ToChar(rnd.Next(10).ToString()),
+                    Convert.ToChar(rnd.Next(10).ToString()),
+                    Convert.ToChar(rnd.Next(10).ToString())
+                };
+                if (randomData.Distinct().Count() == 4)
+                {
+                    return new string(randomData);
+                }
+            }
+        }
     }
 }
